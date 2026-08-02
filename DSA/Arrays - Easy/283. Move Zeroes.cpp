be5847d6a22900d5175f5ff486553919b631c42f
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-02 22:21:02 +0530

    Add function to move zeroes in an array
    Implement a function to move all zeroes in the array to the end while maintaining the order of non-zero elements.

---
+++ DSA/Arrays - Easy/283. Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i,j=0,n=nums.size();
+        for(i=0;i<n;i++) {
+            if(nums[i]!=0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while(j<n) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
