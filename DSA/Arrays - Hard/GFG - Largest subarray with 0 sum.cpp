ba500d9608a37c96f6a315921d6571591a66f62a
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-07 20:36:05 +0530

    Add solution for largest subarray with 0 sum
    Implement a solution to find the largest subarray with a sum of zero.

---
+++ DSA/Arrays - Hard/GFG - Largest subarray with 0 sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+        int n = arr.size();
+        
+        unordered_map<int, int> mp;
+        int sum = 0;
+        int maxlen = 0;
+        
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+            
+            if(sum == 0) {
+                maxlen = i+1;
+            }
+            else if(mp.count(sum)) {
+                maxlen = max(maxlen, i - mp[sum]);
+            }
+            else {
+                mp[sum] = i;
+            }
+        }
+        
+        return maxlen;
+    }
+};
