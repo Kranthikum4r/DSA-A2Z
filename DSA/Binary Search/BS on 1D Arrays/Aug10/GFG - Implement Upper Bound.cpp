28be218d7d5ddebe944e5179a17c8af20f9e732c
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-10 22:21:56 +0530

    Create GFG - Implement Upper Bound.cpp

---
+++ DSA/Binary Search/BS on 1D Arrays/Aug10/GFG - Implement Upper Bound.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        int l = 0, r = arr.size()-1;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(arr[mid] > target) r = mid - 1;
+            else l = mid+1;
+        }
+        return l;
+    }
+};
