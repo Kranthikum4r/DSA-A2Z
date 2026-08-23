ce9b47d1816e1c10c5b23877786ded21bf75b788
Kranthi Kumar Chintha <mrkk1870@gmail.com>
2026-08-23 23:04:01 +0530

    Implement solution for shipping packages within D days

---
+++ DSA/Binary Search/BS on Answers/Aug23/1011. Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canShipped(vector<int>& weights, int capacity, int days) {
+        int reqDays = 1;
+        int currentWeight = 0;
+
+        for(int w : weights) {
+            if(currentWeight + w > capacity) {
+                reqDays++;
+                currentWeight = 0;
+            }
+
+            currentWeight += w;
+
+            if(reqDays > days)
+                return false;
+        }
+
+        return true;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = 0, h = 0;
+        
+        for(int w : weights) {
+            l = max(l, w); // max element
+            h += w; // sum of weights
+        }
+        
+        int ans = h;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if(canShipped(weights, mid, days)) {
+                ans = mid;
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
